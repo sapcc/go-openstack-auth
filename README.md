19acd68532a411179337fd15fb472afe63bf73cd
Arturo Reuschenbach Puncernau <a.reuschenbach.puncernau@sap.com>
2016-06-22 14:54:16 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # go-openstack-auth
 Go openstack auth v3 for getting tokens and endpoints
+
+Example:
+
+		options := auth.AuthV3Options{
+			IdentityEndpoint:  viper.GetString(ENV_VAR_AUTH_URL),
+			Region:            viper.GetString(ENV_VAR_REGION),
+			Username:          viper.GetString(ENV_VAR_USERNAME),
+			UserId:            viper.GetString(ENV_VAR_USER_ID),
+			Password:          viper.GetString(ENV_VAR_PASSWORD),
+			ProjectName:       viper.GetString(ENV_VAR_PROJECT_NAME),
+			ProjectId:         viper.GetString(ENV_VAR_PROJECT_ID),
+			UserDomainName:    viper.GetString(ENV_VAR_USER_DOMAIN_NAME),
+			UserDomainId:      viper.GetString(ENV_VAR_USER_DOMAIN_ID),
+			ProjectDomainName: viper.GetString(ENV_VAR_PROJECT_DOMAIN_NAME),
+			ProjectDomainId:   viper.GetString(ENV_VAR_PROJECT_DOMAIN_ID),
+		}
+
+		authV3 := auth.New(options)
+		token, err := authV3.GetToken()
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		fmt.Println("°°°TOKEN°°°")
+		fmt.Println(token.ID)
+		fmt.Println("°°°")
+
+		endpoint, err := authV3.GetServiceEndpoint("arc", "staging", "public")
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		fmt.Println("°°°ENDPOINT°°°")
+		fmt.Println(endpoint)
+		fmt.Println("°°°")
---
+++ identityV3.go
@@ -0,0 +1,167 @@
+package goOpenstackAuth
+
+import (
+	"fmt"
+
+	"github.com/rackspace/gophercloud"
+	"github.com/rackspace/gophercloud/openstack"
+	"github.com/rackspace/gophercloud/openstack/identity/v3/tokens"
+)
+
+type AuthV3Options struct {
+	IdentityEndpoint  string
+	Region            string
+	Username          string
+	UserId            string
+	Password          string
+	ProjectName       string
+	ProjectId         string
+	UserDomainName    string
+	UserDomainId      string
+	ProjectDomainName string
+	ProjectDomainId   string
+}
+
+type Authentication interface {
+	GetToken() (*tokens.Token, error)
+	GetServiceEndpoint(serviceType, region, serviceInterface string) (string, error)
+}
+
+type AuthV3 struct {
+	Options     AuthV3Options
+	tokenResult *tokens.CreateResult
+	client      *gophercloud.ServiceClient
+}
+
+func New(authOpts AuthV3Options) Authentication {
+	return &AuthV3{Options: authOpts}
+}
+
+func (a *AuthV3) GetToken() (*tokens.Token, error) {
+	var err error
+	err = a.createTokenCommonResult()
+	if err != nil {
+		return nil, err
+	}
+
+	token, err := a.tokenResult.Extract()
+	if err != nil {
+		return nil, err
+	}
+
+	return token, nil
+}
+
+func (a *AuthV3) GetServiceEndpoint(serviceType, region, serviceInterface string) (string, error) {
+	if a.tokenResult == nil {
+		a.GetToken()
+	}
+
+	// get catalog
+	catalog, err := a.tokenResult.ExtractServiceCatalog()
+	if err != nil {
+		return "", err
+	}
+
+	// get entry from catalog
+	serviceEntry, err := getServiceEntry(serviceType, catalog)
+	if err != nil {
+		return "", err
+	}
+
+	// get endpoint
+	endpoint, err := getServiceEndpoint(region, serviceInterface, serviceEntry)
+	if err != nil {
+		return "", err
+	}
+
+	return endpoint, nil
+}
+
+func (a *AuthV3) getAuthOptions() gophercloud.AuthOptions {
+	return gophercloud.AuthOptions{
+		IdentityEndpoint: a.Options.IdentityEndpoint,
+		Username:         a.Options.Username,
+		UserID:           a.Options.UserId,
+		Password:         a.Options.Password,
+		DomainName:       a.Options.UserDomainName,
+		DomainID:         a.Options.UserDomainId,
+	}
+}
+
+func (a *AuthV3) getClient() (*gophercloud.ServiceClient, error) {
+	// get provider client struct
+	provider, err := openstack.AuthenticatedClient(a.getAuthOptions())
+	if err != nil {
+		return nil, err
+	}
+	return openstack.NewIdentityV3(provider), nil
+}
+
+func (a *AuthV3) createTokenCommonResult() error {
+	scope := tokens.Scope{
+		ProjectName: a.Options.ProjectName,
+		ProjectID:   a.Options.ProjectId,
+		DomainName:  a.Options.ProjectDomainName,
+		DomainID:    a.Options.ProjectDomainId,
+	}
+
+	// init the v3 client
+	var err error
+	if a.client == nil {
+		a.client, err = a.getClient()
+		if err != nil {
+			return err
+		}
+	}
+
+	// create common result
+	result := tokens.Create(a.client, a.getAuthOptions(), &scope)
+
+	// save common result
+	a.tokenResult = &result
+
+	return nil
+}
+
+// private
+
+func getServiceEndpoint(region string, serviceInterface string, entry *tokens.CatalogEntry) (string, error) {
+	if entry != nil && len(entry.Endpoints) > 0 {
+		var endpoint string
+		for _, ep := range entry.Endpoints {
+			if region != "" {
+				if ep.Interface == serviceInterface && ep.Region == region {
+					endpoint = ep.URL
+					break
+				}
+			} else {
+				if ep.Interface == serviceInterface {
+					endpoint = ep.URL
+					break
+				}
+			}
+		}
+		return endpoint, nil
+	} else {
+		return "", fmt.Errorf("Authenticate: getServicePublicEndpoint: entry nil or no endpoints found for %+v.", entry)
+	}
+	return "", nil
+}
+
+func getServiceEntry(serviceType string, catalog *tokens.ServiceCatalog) (*tokens.CatalogEntry, error) {
+	if catalog != nil && len(catalog.Entries) > 0 {
+		serviceEntry := tokens.CatalogEntry{}
+		for _, service := range catalog.Entries {
+			if service.Type == serviceType {
+				serviceEntry = service
+				break
+			}
+		}
+		return &serviceEntry, nil
+	} else {
+		return nil, fmt.Errorf("Authenticate: GetServicePublicEndpoint: catalog nil or emtpy.")
+	}
+
+	return nil, nil
+}
