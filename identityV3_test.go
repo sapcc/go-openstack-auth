3fe8ec2972fc9d2fa2346fc76aebb47a84454584
Arturo Reuschenbach Puncernau <a.reuschenbach.puncernau@sap.com>
2016-06-24 11:09:48 +0200

    refactoring

--- identityV3_test.go
+++ identityV3_test.go
@@ -4,12 +4,10 @@ import (
 	"fmt"
 	"strings"
 	"testing"
-
-	"github.com/rackspace/gophercloud/openstack/identity/v3/tokens"
 )
 
 func resetAuthentication() {
-	AuthenticationV3 = newMockAuthenticationV3
+	AuthenticationV3 = NewMockAuthenticationV3
 }
 
 func TestAuthenticationTokenSuccess(t *testing.T) {
@@ -163,59 +161,3 @@ func TestAuthenticationProject(t *testing.T) {
 		t.Error(fmt.Sprintf("Project id does not match. \n \n %s", diffString))
 	}
 }
-
-//
-// Mock authentication interface
-//
-
-type MockV3 struct {
-	Options     AuthV3Options
-	tokenResult *tokens.CreateResult
-}
-
-func newMockAuthenticationV3(authOpts AuthV3Options) Authentication {
-	return &MockV3{Options: authOpts}
-}
-
-func (a *MockV3) GetToken() (*tokens.Token, error) {
-	token := tokens.Token{ID: "test_token_id"}
-	return &token, nil
-}
-
-func (a *MockV3) GetServiceEndpoint(serviceType, region, serviceInterface string) (string, error) {
-	// get entry from catalog
-	serviceEntry, err := getServiceEntry(serviceType, &catalog1)
-	if err != nil {
-		return "", err
-	}
-
-	// get endpoint
-	endpoint, err := getServiceEndpoint(region, serviceInterface, serviceEntry)
-	if err != nil {
-		return "", err
-	}
-
-	return endpoint, nil
-}
-
-func (a *MockV3) GetProject() (*Project, error) {
-	return extractProject(commonResult)
-}
-
-var catalog1 = tokens.ServiceCatalog{
-	Entries: []tokens.CatalogEntry{
-		{ID: "s-8be070817", Name: "Arc", Type: "arc", Endpoints: []tokens.Endpoint{
-			{ID: "e-904f431c9", Region: "staging", Interface: "internal", URL: "https://arc-app-staging/internal"},
-			{ID: "e-904f431c9", Region: "staging", Interface: "admin", URL: "https://arc-app-staging/admin"},
-			{ID: "e-884f431c9", Region: "staging", Interface: "public", URL: "https://arc-app-staging/public"},
-			{ID: "e-904f431c9", Region: "production", Interface: "internal", URL: "https://arc-app-prod/internal"},
-			{ID: "e-904f431c9", Region: "production", Interface: "admin", URL: "https://arc-app-prod/admin"},
-			{ID: "e-884f431c9", Region: "production", Interface: "public", URL: "https://arc-app-prod/public"},
-		}},
-		{ID: "s-d5e793744", Name: "Lyra", Type: "automation", Endpoints: []tokens.Endpoint{
-			{ID: "e-54b8d28fc", Region: "staging", Interface: "public", URL: "https://lyra-app"},
-		}},
-	},
-}
-
-var commonResult = map[string]interface{}{"token": map[string]interface{}{"project": map[string]string{"id": "p-9597d2775", "domain_id": "o-monsoon2", "name": "Arc_Development"}}}
